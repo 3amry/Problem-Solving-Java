d9d0aa1efe3ca41a4f9fac1141e65ad6cb8f77b2
3amry <74096502+3amry@users.noreply.github.com>
2023-08-01 08:53:26 +0200

    String: Valid Parentheses

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
-import leetcode.strings.LongestCommonPrefix.LongestCommonPrefix;
+import leetcode.strings.ValidParentheses.ValidParentheses;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome To Problem Solving In Java!");
 
-        System.out.println(
-        LongestCommonPrefix.longestPrefix(new String[]{"flower", "flow", "flight"})
-        );
-        System.out.println(
-        LongestCommonPrefix.longestPrefix(new String[]{"dog","racecar","car"})
-        );
-        System.out.println(
-        LongestCommonPrefix.longestPrefix(new String[]{"a", "a"})
-        );
-        System.out.println(
-        LongestCommonPrefix.longestPrefix(new String[]{"ab", "a"})
-        );
+        System.out.println(ValidParentheses.validate("()"));
+        System.out.println(ValidParentheses.validate("()[]{}"));
+        System.out.println(ValidParentheses.validate("{[]}"));
+        System.out.println(ValidParentheses.validate("(]"));
+        System.out.println(ValidParentheses.validate("["));
     }
 }
---
+++ src/leetcode/strings/ValidParentheses/ValidParentheses.java
@@ -0,0 +1,23 @@
+package leetcode.strings.ValidParentheses;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    private static Stack<Character> stack = new Stack<>();
+    public static boolean validate(String sample){
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        var leftBrackets = map.keySet();
+        for (var ch: sample.toCharArray()){
+            if (leftBrackets.contains(ch))
+                stack.push(ch);
+            else if (stack.isEmpty() || map.get(stack.pop()) != ch)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
